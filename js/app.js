eeeb5d17ff4cd516055018cb4b30d558aaab8d8f
MatthewPass24 <matthewpass24@gmail.com>
2024-05-13 10:11:18 -0400

    finished initial html elements and started js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+
+const dateElement = document.getElementById("date");
+
+const list = document.getElementById("list")
+    const text = <li class = "item">
+                    <i class="co fa fa-circle-thin" job="complete"></i>
+                    <p class="text">Drink Coffee</p>
+                    <i class="de fa fa-trash-o" job="delete"></i>
+                </li>
+
+const input = document.getElementById("input")
